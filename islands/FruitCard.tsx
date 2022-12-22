74ffcfe54cc71fc99bbde1acbcf7f032fa0e6dec
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-22 21:21:56 +0100

    Added content and functionality

--- islands/FruitCard.tsx
+++ islands/FruitCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "preact/hooks";
+
 interface Fruit {
 	genus: string;
 	name: string;
@@ -16,10 +18,24 @@ interface Nutritions {
 }
 
 const FruitCard = (fruit: Fruit) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
 	return (
-		<div>
-			<p>{fruit.name}</p>
-			<p>{fruit.nutritions.protein}</p>
+		<div class="w-56">
+			<div class="flex">
+				<h1 class="text-3xl">{fruit.name}</h1>
+				<button onClick={() => setIsOpen(!isOpen)}>{isOpen ? "-" : "+"}</button>
+			</div>
+			<h3>Family: {fruit.family}</h3>
+			{isOpen && (
+				<ul>
+					<li>Carbs: {fruit.nutritions.carbohydrates}g</li>
+					<li>Sugar: {fruit.nutritions.sugar}g</li>
+					<li>Fats: {fruit.nutritions.fat}g</li>
+					<li>Protein: {fruit.nutritions.protein}g</li>
+					<li>Calories: {fruit.nutritions.calories}g</li>
+				</ul>
+			)}
 		</div>
 	);
 };
