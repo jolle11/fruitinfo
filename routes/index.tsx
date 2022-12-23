aa76720f4299c58190584367dc876d93ff5c4ef0
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-12-23 14:06:46 +0100

    Added Data interface, implemented search and a bit of spacing between elements

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,6 +2,12 @@ import { Head } from "$fresh/runtime.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import FruitCard from "../islands/FruitCard.tsx";
 
+interface Data {
+	fruits: Fruit[];
+	query: string;
+	searchResults: Fruit[];
+}
+
 interface Fruit {
 	genus: string;
 	name: string;
@@ -20,17 +26,25 @@ interface Nutritions {
 }
 
 export const handler: Handlers<Fruit[]> = {
-	async GET(_req, ctx) {
+	async GET(req, ctx) {
 		const resp = await fetch("https://www.fruityvice.com//api/fruit/all");
+
 		if (!resp) {
 			return new Response("No fruits found", { status: 404 });
 		}
+
+		const url = new URL(req.url);
 		const fruits: Fruit[] = await resp.json();
-		return ctx.render(fruits);
+		const query = url.searchParams.get("search") || "";
+		const searchResults = fruits.filter((fruit) => fruit.name.includes(query));
+
+		return ctx.render({ searchResults, query, fruits });
 	},
 };
 
-const Home = ({ data }: PageProps<Fruit[]>) => {
+const Home = ({ data }: PageProps<Data>) => {
+	const { searchResults, query, fruits } = data;
+
 	if (!data) {
 		return <h1>No fruits sorry</h1>;
 	}
@@ -40,15 +54,28 @@ const Home = ({ data }: PageProps<Fruit[]>) => {
 			<Head>
 				<title>Fruitinfo: Fruits information</title>
 			</Head>
-			<div class="p-4 mx-auto max-w-screen-md">
-				<h1>Search Fruits</h1>
-				<input type="text" class="border-2 rounded" />
-			</div>
-			<div class="flex flex-wrap justify-evenly mx-auto">
-				{data.map((fruit) => (
-					<FruitCard {...fruit} />
-				))}
-			</div>
+			<form class="px-36 py-12 mx-auto flex flex-col items-center">
+				<h1 class="text-3xl">Search Fruits</h1>
+				<input
+					type="text"
+					name="search"
+					class="border-2 rounded w-48"
+					value={query}
+				/>
+			</form>
+			{searchResults.length ? (
+				<div class="flex flex-wrap justify-evenly mx-auto">
+					{searchResults.map((fruit) => (
+						<FruitCard {...fruit} />
+					))}
+				</div>
+			) : (
+				<div class="flex flex-wrap justify-evenly mx-auto">
+					{fruits.map((fruit) => (
+						<FruitCard {...fruit} />
+					))}
+				</div>
+			)}
 		</>
 	);
 };
